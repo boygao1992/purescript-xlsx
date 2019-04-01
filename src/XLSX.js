ed848b74def40ab5c44b59319d703e90b5270276
boygao1992 <wg2313@columbia.edu>
2019-04-01 14:34:34 -0400

    init

---
+++ src/XLSX.js
@@ -0,0 +1,24 @@
+"use strict"
+
+var xlsx = require( "xlsx" )
+
+exports._toCSV = function _toCSV( Left, Right, Tuple ) {
+  return function ( content ) {
+    try {
+      var parsed = xlsx.read( content, { type: "base64" } )
+    } catch ( e ) {
+      return Left( "Unrecognized encoding: XLSX in Base64 expected." )
+    }
+
+    var sheets = []
+    for ( var sheetName in parsed.Sheets ) {
+      sheets.push(
+        Tuple(
+          sheetName,
+          xlsx.utils.sheet_to_csv( parsed.Sheets[ sheetName ] )
+        )
+      )
+    }
+    return Right( sheets )
+  }
+}
---
+++ src/XLSX.purs
@@ -0,0 +1,25 @@
+module XLSX where
+
+import Prelude
+
+import Data.Either (Either(..))
+import Data.Function.Uncurried (Fn2, Fn3, mkFn2, runFn3)
+import Data.Map (Map, fromFoldable)
+import Data.Tuple (Tuple(..))
+import Effect (Effect)
+import Effect.Uncurried (EffectFn1, runEffectFn1)
+
+foreign import _toCSV ::
+  Fn3
+    (String -> Either String String)
+    (String -> Either String String)
+    (Fn2 String String (Tuple String String))
+    (EffectFn1
+       String
+       (Either String (Array (Tuple String String)))
+    )
+
+-- NOTE content has to be encoded in Base64
+toCSV :: String -> Effect (Either String (Map String String))
+toCSV content =
+  map fromFoldable <$> runEffectFn1 (runFn3 _toCSV Left Right (mkFn2 Tuple)) content
